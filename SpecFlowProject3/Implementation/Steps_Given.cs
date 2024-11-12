8a5a2f31283867ca9f8ccfa1f89cef15b16d12bc
Andreas Lauvhjell <andreas.lauvhjell@nov.com>
2024-11-12 09:50:53 +0100

    10 ferdig

--- SpecFlowProject3/Implementation/Steps_Given.cs
+++ SpecFlowProject3/Implementation/Steps_Given.cs
@@ -10,18 +10,25 @@ using TechTalk.SpecFlow.UnitTestProvider;
 namespace SpecFlowProject3.Implementation
 {
     [Binding]
-    public class Steps_Given : ContextHelper
+    public class Steps_Given
     {
+        private ProductTestDataContext _productTestDataContext;
+
+        public Steps_Given(ProductTestDataContext productTestDataContext)
+        {
+            _productTestDataContext = productTestDataContext;
+        }
+
         [Given(@"I have the following data")]
         public void GivenIHaveTheFollowingData(Table table)
         {
-            ProductContext.ProductWithQuantities = table.CreateSet<ProductQuantities>();
+            _productTestDataContext.ProductWithQuantities = table.CreateSet<ProductQuantities>();
         }
 
         [Given(@"I am on the product detail page of product (.*)")]
         public void GivenIAmOnTheProductDetailPageOfProduct(int productId)
         {
-            ProductContext.TestingProduct = ProductContext.ProductWithQuantities.FirstOrDefault(t => int.Parse(t.ProductID) == productId);
+            _productTestDataContext.TestingProduct = _productTestDataContext.ProductWithQuantities.FirstOrDefault(t => int.Parse(t.ProductID) == productId);
 
         }
     }
--- SpecFlowProject3/Implementation/Steps_Then.cs
+++ SpecFlowProject3/Implementation/Steps_Then.cs
@@ -8,12 +8,19 @@ using TechTalk.SpecFlow.Assist;
 namespace SpecFlowProject3.Implementation
 {
     [Binding]
-    public class Steps_Then : ContextHelper
+    public class Steps_Then
     {
+        private ProductTestDataContext _productTestDataContext;
+
+        public Steps_Then(ProductTestDataContext productTestDataContext)
+        {
+            _productTestDataContext = productTestDataContext;
+        }
+
         [Then(@"the quantities are")]
         public void ThenTheQuantitiesAre(Table table)
         {
-            var comparisonList = ProductContext.ProductWithQuantities.Where(t => t.ProductID == ProductContext.TestingProduct.ProductID);
+            var comparisonList = _productTestDataContext.ProductWithQuantities.Where(t => t.ProductID == _productTestDataContext.TestingProduct.ProductID);
             table.CompareToSet<ProductQuantities>(comparisonList);
         }
 
